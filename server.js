acd324448b5543cd92a98b93c6fda70d64d1457f
sohamarora1 <148689103+sohamarora1@users.noreply.github.com>
2024-11-29 23:52:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ import addressRouter from "./Routes/address.js";
 import cors from 'cors';
 
 const app = express();
-
+app.use(express.json());
 app.use(cors({
   origin:true,
   methods:[ "GET","POST","PUT","DELETE"],
   credentials:true
 }));
-app.use(express.json());
+
 
 
 
