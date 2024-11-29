9f58e226146a2f4520f46ebc6b6ad959db0388b1
sohamarora1 <148689103+sohamarora1@users.noreply.github.com>
2024-11-30 00:05:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ import productRouter from "./Routes/product.js";
 import cartRouter from "./Routes/cart.js";
 import addressRouter from "./Routes/address.js";
 import cors from 'cors';
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use(express.json());
+
+// CORS configuration
 app.use(cors({
-  origin:true,
-  methods:[ "GET","POST","PUT","DELETE"],
-  credentials:true
+    origin: ['https://adorafrontend.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Auth', 'Origin', 'X-Requested-With', 'Accept']
 }));
 
+// Other middleware
+app.use(express.json());
 
 
 
