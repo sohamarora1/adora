68a238209e1269088eaba8d02dd73d4a00b76c69
sohamarora1 <148689103+sohamarora1@users.noreply.github.com>
2024-11-30 11:38:57 +0530

    Update user.js

--- Routes/user.js
+++ Routes/user.js
@@ -1,14 +1,19 @@
-import express from 'express';
-import { 
-    registerUser, 
-    loginUser, 
-    validateToken 
-} from '../Controllers/user.js';
+import express from "express";
+import { login, profile, register, users } from "../Controllers/user.js";
+import { Authenticated } from "../Middlewares/auth.js";
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
-router.post('/validate-token', validateToken); // New validation endpoint
+//register user
+router.post("/register", register);
+
+//login user
+router.post("/login", login);
+
+//get all users
+router.get("/all", users);
+
+//get profile
+router.get("/profile",Authenticated, profile);
 
 export default router;
