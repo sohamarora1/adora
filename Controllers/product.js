fad3e6697faeebe20933936a9052c5482e5d6995
sohamarora1 <148689103+sohamarora1@users.noreply.github.com>
2024-12-14 18:42:21 +0530

    Update product.js

--- Controllers/product.js
+++ Controllers/product.js
@@ -2,13 +2,14 @@ import { Products } from "../Models/Product.js";
 
 // add product
 export const addProduct = async (req,res) =>{
-    const {title,description,price,category,qty,imgSrc} = req.body
+    const {title,description,price,category,type,qty,imgSrc} = req.body
     try {
         let product = await Products.create({
           title,
           description,
           price,
           category,
+          type,
           qty,
           imgSrc,
         });
